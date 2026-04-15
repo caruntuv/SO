41d08d57edeee6c6d32540d27e220288a43c5657
student <vlad.caruntu@student.upt.ro>
2026-04-15 09:25:07 +0300

    mdkfdkf

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 #include<stdio.h>
+#include<string.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-  printf("Cucubau\n");
+  if (argc<5)
+    {
+      printf("Usage: ./city_manager --role <role> --user <name> <command>\n");
+      return 1;
+    }
+  char role[20];
+  char user[50];
+
+  for(int i=1;i<argc;i++)
+    {
+      if (strcmp(argv[i], "--role") == 0)
+	{
+	  strcpy(role,argv[i+1]);
+	}
+         if (strcmp(argv[i], "--user") == 0)
+	{
+	  strcpy(user,argv[i+1]);
+	}
+    }
+  printf("Role: %s\n", role);
+  printf("User: %s\n", user);
+  
   return 0;
 }
